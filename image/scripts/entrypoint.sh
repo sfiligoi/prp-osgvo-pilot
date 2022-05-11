174c385eea02a54d175b0e3bc026379dfb453024
Igor Sfiligoi <isfiligoi@ucsd.edu>
2022-05-11 16:03:03 -0700

    Set HOME to /pilot

--- image/scripts/entrypoint.sh
+++ image/scripts/entrypoint.sh
@@ -3,4 +3,5 @@
 # Allow the derived images to run any additional runtime customizations
 for x in /etc/entrypoint/image-config.d/*.sh; do source "$x"; done
 
+export HOME=/pilot
 su osg -p -c "/bin/entrypoint.osg.sh $@"
