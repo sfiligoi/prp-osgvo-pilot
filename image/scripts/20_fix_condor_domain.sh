a39eacb6b7047aa7bb9e0d1666faf49892d0bff4
Igor Sfiligoi <isfiligoi@ucsd.edu>
2022-05-12 10:17:29 -0700

    Do not use domain for NETWORK_HOSTNAME, HTCondor doe snot like it

--- image/scripts/20_fix_condor_domain.sh
+++ image/scripts/20_fix_condor_domain.sh
@@ -1,9 +1,16 @@
 #!/bin/bash
 
-if [ "x${DOMAIN}" == "x" ]; then
-  # should nover get in here, but just in case
-  DOMAIN=local
-fi
 
-echo "NETWORK_HOSTNAME = \$(NETWORK_HOSTNAME).${DOMAIN}" >> "${PILOT_CONFIG_FILE}"
+#
+# Embed the namespace and physical hostname in the condor name
+#
+
+if [ "x${K8S_NAMESPACE}" != "x" ]; then
+  echo "NETWORK_HOSTNAME = \$(NETWORK_HOSTNAME)-${K8S_NAMESPACE}" >> "${PILOT_CONFIG_FILE}"
+fi
+if [ "x${PHYSICAL_HOSTNAME}" != "x" ]; then
+  # let's keep . for separators only
+  hstr=`echo "${PHYSICAL_HOSTNAME}" | tr '.' '-'`
+  echo "NETWORK_HOSTNAME = \$(NETWORK_HOSTNAME)-${hstr}" >> "${PILOT_CONFIG_FILE}"
+fi
 
