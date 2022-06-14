a2b9fd95fc9eb73c6878ccecb61b114e0dd4b37a
Igor Sfiligoi <isfiligoi@ucsd.edu>
2022-06-14 10:24:15 -0700

    Keep shared port but use dynamic value

--- image-lancium/scripts/31_fix_shared_config.sh
+++ image-lancium/scripts/31_fix_shared_config.sh
@@ -1,4 +1,4 @@
 #
 # Shared port daemon expects a fixed port, not appropriate for a pilot
 #
-echo "USE_SHARED_PORT=false" >> "${PILOT_CONFIG_FILE}"
+echo "SHARED_PORT_PORT=0" >> "${PILOT_CONFIG_FILE}"
