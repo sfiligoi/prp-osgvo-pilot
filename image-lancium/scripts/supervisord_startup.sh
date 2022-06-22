0c5afa1b1d8300cb6b978c6cd90c521bcf492be3
Igor Sfiligoi <isfiligoi@ucsd.edu>
2022-06-22 15:09:26 -0700

    Remove any CONDOR_HOST setting

--- image-lancium/scripts/supervisord_startup.sh
+++ image-lancium/scripts/supervisord_startup.sh
@@ -4,10 +4,6 @@
 export SINGULARITY_BIND=
 unset SINGULARITY_BIND
 
-# expect the token in a fixed location
-export TOKEN=`cat prp-wn.token`
-rm -f prp-wn.token
-
 # convert all the arguments to env variables
 while [[ $1 ]]
 do
@@ -18,5 +14,12 @@ do
     export "$k"=$"$v"
 done
 
+# expect the token in a fixed location
+export TOKEN=`cat prp-wn.token`
+rm -f prp-wn.token
+
+# OSGVO pilot does not like it when one sets the CONDOR_HOST env
+export CONDOR_HOST=
+unset CONDOR_HOST
 
 exec /usr/local/sbin/supervisord_startup.osg.sh
