d71e16be5eff4d06a23858a6955ce7acfa0dfa4e
Igor Sfiligoi <isfiligoi@ucsd.edu>
2022-06-22 17:02:29 -0700

    Switch from STARTD_ATTRS to STARTD_EXPRS

--- image-lancium/scripts/21_advertise_lancium_provisioner.sh
+++ image-lancium/scripts/21_advertise_lancium_provisioner.sh
@@ -7,17 +7,17 @@
 
 if [ "x${LANCIUM_PROVISIONER_TYPE}" != "x" ]; then
   echo "LanciumProvisionerType=\"${LANCIUM_PROVISIONER_TYPE}\"" >> "${PILOT_CONFIG_FILE}"
-  echo 'STARTD_ATTRS = $(STARTD_ATTRS), LanciumProvisionerType'  >> "${PILOT_CONFIG_FILE}"
+  echo 'STARTD_EXPRS = $(STARTD_EXPRS) LanciumProvisionerType'  >> "${PILOT_CONFIG_FILE}"
 fi
 
 if [ "x${LANCIUM_PROVISIONER_NAME}" != "x" ]; then
   echo "LanciumProvisionerName=\"${LANCIUM_PROVISIONER_NAME}\"" >> "${PILOT_CONFIG_FILE}"
-  echo 'STARTD_STTRS = $(STARTD_ATTRS), LanciumProvisionerName'  >> "${PILOT_CONFIG_FILE}"
+  echo 'STARTD_STTRS = $(STARTD_EXPRS) LanciumProvisionerName'  >> "${PILOT_CONFIG_FILE}"
 fi
 
 if [ "x${LANCIUM_JOB_NAME}" != "x" ]; then
   echo "LanciumJobName=\"${LANCIUM_JOB_NAME}\"" >> "${PILOT_CONFIG_FILE}"
-  echo 'STARTD_ATTRS = $(STARTD_ATTRS), LanciumJobName'  >> "${PILOT_CONFIG_FILE}"
+  echo 'STARTD_EXPRS = $(STARTD_EXPRS) LanciumJobName'  >> "${PILOT_CONFIG_FILE}"
   #also inject job name into the startd name
   sed -i "s/\(NETWORK_HOSTNAME = .*\)-$(hostname)/\1-${LANCIUM_JOB_NAME}-$(hostname)/" "${PILOT_CONFIG_FILE}"
 fi
